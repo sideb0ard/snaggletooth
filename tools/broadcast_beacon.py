d4d5cdbaf01f7b7430934d619f2b40b9e77c1fa6
Thor <thorsten.sideb0ard@gmail.com>
2016-07-26 22:17:47 -0700

    finishing this broadcast_beacon

--- tools/broadcast_beacon.py
+++ tools/broadcast_beacon.py
@@ -4,39 +4,45 @@
 # http://yencarnacion.github.io/eddystone-url-calculator/ and
 # https://hackaday.io/project/10314-raspberry-pi-3-as-an-eddystone-url-beacon
 
+import os
 import sys
 import subprocess
 
 
-def encodeUrl(url):
-    # see links in header for details of these fields
+def sendBeacon():
+
+    uid =  os.getuid()
+    if uid != 0:
+	print "Sorry, pal, need to be superuser to change hci settings"
+	exit(1)
+
+    ip = subprocess.check_output("/sbin/ifconfig wlan0 | grep 'inet addr' | "
+                                 "awk '{print $2}' | sed 's/addr://'",
+				 shell=True)
+    ip = ip.rstrip('\n')
+
+    ## see links in header for details of these fields
     prefix = "0x08 0x0008 17 02 01 06 03 03 aa fe 0f 16 aa fe 10 00 02 "
-    print "I got yer url:{0}".format(url)
-    url = url.encode("hex")
+    url = ip.encode("hex")
     if len(url) > 34:
         print "Yer URL is too long mate"
         exit(1)
-    print url
-    print len(url)
 
     while len(url) < 34:
         url = url + "00"
 
-    print len(url)
-
     url = " ".join(url[i:i + 2] for i in range(0, len(url), 2))
-    output = prefix + url
+    encoded_params = prefix + url
 
-    ip = subprocess.check_output(["/sbin/ifconfig wlan0 | grep 'inet addr' | "
-                                  "awk '{print $2}' | sed 's/addr://'"])
+    print "{0}".format(encoded_params)
 
-    print "IP: {0}".format(ip)
-    print "Output: {0}".format(output)
+    print "Putting hci0 in broadcast mode..."
+    subprocess.check_call(["hciconfig", "hci0", "reset"])
+    subprocess.check_call(["hciconfig", "hci0", "leadv", "3"])
+
+    cmd = "hcitool -i hci0 cmd " + encoded_params
+    subprocess.check_output(cmd, shell=True) 
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        print "Yarly!"
-        encodeUrl(sys.argv[1])
-    else:
-        print "boo"
+    sendBeacon()
