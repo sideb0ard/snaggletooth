182dc8f0223542dc3155a55d76eb45813f22a202
tack <tackyy@gmail.com>
2016-07-28 20:34:49 -0700

    initial gpio test script

---
+++ garbage/gpio/helloblink.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+channel = 6
+
+GPIO.setup(channel, GPIO.OUT, initial=GPIO.HIGH)
+
+while True:
+    GPIO.output(channel, 0)
+    time.sleep(1)
+    GPIO.output(channel, 1)
+    time.sleep(1)
