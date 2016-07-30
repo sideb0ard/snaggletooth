b05cf19e4cf2780f8a69795dc318cd6d942d8823
Thor Sideburns <thorsten.sideb0ard@gmail.com>
2016-07-29 21:07:28 -0700

    first pass at Android app that scans for and broadcasts BLE tokens

---
+++ android-app/app/src/main/java/com/theb0ardside/k9/MainActivity.java
@@ -0,0 +1,209 @@
+package com.theb0ardside.k9;
+
+import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.AdvertiseCallback;
+import android.bluetooth.le.AdvertiseData;
+import android.bluetooth.le.AdvertiseSettings;
+import android.bluetooth.le.BluetoothLeAdvertiser;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.os.ParcelUuid;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mText;
+    private Button mAdvertiseButton;
+    private Button mDiscoverButton;
+
+    private BluetoothLeScanner mBluetoothLeScanner;
+    private Handler mHandler = new Handler();
+
+    public static final int REQUEST_LINE_CODE = 74;
+
+    private ScanCallback mScanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            super.onScanResult(callbackType, result);
+            if( result == null
+                    || result.getDevice() == null
+                    || TextUtils.isEmpty(result.getDevice().getName()) )
+                return;
+
+            StringBuilder builder = new StringBuilder( result.getDevice().getName() );
+
+            builder.append("\n").append(new String(result.getScanRecord().getServiceData(result.getScanRecord().getServiceUuids().get(0)), Charset.forName("UTF-8")));
+
+            mText.setText(builder.toString());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            super.onBatchScanResults(results);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            Log.e( "BLE", "Discovery onScanFailed: " + errorCode );
+            super.onScanFailed(errorCode);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mText = (TextView) findViewById( R.id.text );
+        mDiscoverButton = (Button) findViewById( R.id.discover_btn );
+        mAdvertiseButton = (Button) findViewById( R.id.advertise_btn );
+
+        mDiscoverButton.setOnClickListener( this );
+        mAdvertiseButton.setOnClickListener( this );
+
+        if( !BluetoothAdapter.getDefaultAdapter().isMultipleAdvertisementSupported() ) {
+            Toast.makeText( this, "Multiple advertisement not supported", Toast.LENGTH_SHORT ).show();
+            mAdvertiseButton.setEnabled( false );
+            mDiscoverButton.setEnabled( false );
+        }
+
+        int hasLocationPermission = checkSelfPermission( Manifest.permission.ACCESS_FINE_LOCATION );
+        int hasBluetoothPermission = checkSelfPermission( Manifest.permission.BLUETOOTH );
+        int hasBluetoothAdminPermission = checkSelfPermission( Manifest.permission.BLUETOOTH_ADMIN );
+
+        List<String> permissions = new ArrayList<String>();
+
+        Log.d( "THOR", "has lcoation perms? " + hasLocationPermission);
+        Log.d( "THOR", "has bluetooth perms? " + hasBluetoothPermission);
+        Log.d( "THOR", "has blutooth_admin perms? " + hasBluetoothAdminPermission);
+
+
+        if( hasLocationPermission != PackageManager.PERMISSION_GRANTED ) {
+
+            permissions.add( Manifest.permission.ACCESS_FINE_LOCATION );
+            Log.d( "THOR", "nae location!");
+        }
+        if( hasBluetoothPermission != PackageManager.PERMISSION_GRANTED ) {
+
+            permissions.add( Manifest.permission.BLUETOOTH );
+            Log.d( "THOR", "nae bluetooth!");
+        }
+        if( hasBluetoothAdminPermission != PackageManager.PERMISSION_GRANTED ) {
+
+            permissions.add( Manifest.permission.BLUETOOTH_ADMIN );
+            Log.d( "THOR", "nae bluetooth admin!");
+        }
+
+        Log.d( "THOR", "perm size! " + permissions.size() );
+        if (!permissions.isEmpty()) {
+            Log.d( "THOR", "perm size! " + permissions.size() );
+            requestPermissions(permissions.toArray(new String[permissions.size()]), REQUEST_LINE_CODE );
+        }
+
+        mBluetoothLeScanner = BluetoothAdapter.getDefaultAdapter().getBluetoothLeScanner();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch ( requestCode ) {
+            case REQUEST_LINE_CODE: {
+                for( int i = 0; i < permissions.length; i++ ) {
+                    if( grantResults[i] == PackageManager.PERMISSION_GRANTED ) {
+                        Log.d( "Permissions", "Permission Granted: " + permissions[i] );
+                    } else if( grantResults[i] == PackageManager.PERMISSION_DENIED ) {
+                        Log.d( "Permissions", "Permission Denied: " + permissions[i] );
+                    }
+                }
+            }
+            break;
+            default: {
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v.getId() == R.id.discover_btn ) {
+            discover();
+        } else if( v.getId() == R.id.advertise_btn ) {
+            advertise();
+        }
+    }
+
+    public void discover() {
+
+        List<ScanFilter> filters = new ArrayList<ScanFilter>();;
+        ScanFilter filter = new ScanFilter.Builder()
+                .setServiceUuid( new ParcelUuid(UUID.fromString( getString(R.string.ble_uuid ) ) ) )
+                .build();
+        filters.add( filter );
+
+        ScanSettings settings = new ScanSettings.Builder()
+                .setScanMode( ScanSettings.SCAN_MODE_LOW_LATENCY )
+                .build();
+
+        mBluetoothLeScanner.startScan(filters, settings, mScanCallback);
+
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mBluetoothLeScanner.stopScan(mScanCallback);
+            }
+        }, 10000);
+    }
+
+    public void advertise() {
+
+        BluetoothLeAdvertiser advertiser = BluetoothAdapter.getDefaultAdapter().getBluetoothLeAdvertiser();
+
+        AdvertiseSettings settings = new AdvertiseSettings.Builder()
+                .setAdvertiseMode( AdvertiseSettings.ADVERTISE_MODE_LOW_LATENCY )
+                .setTxPowerLevel( AdvertiseSettings.ADVERTISE_TX_POWER_HIGH )
+                .setConnectable( false )
+                .build();
+
+        ParcelUuid pUuid = new ParcelUuid( UUID.fromString( getString( R.string.ble_uuid ) ) );
+
+        AdvertiseData data = new AdvertiseData.Builder()
+                .addServiceUuid( pUuid )
+                .addServiceData( pUuid, "pop".getBytes( Charset.forName( "UTF-8" ) ) )
+                .build();
+
+        AdvertiseCallback advertisingCallback = new AdvertiseCallback() {
+            @Override
+            public void onStartSuccess(AdvertiseSettings settingsInEffect) {
+                super.onStartSuccess(settingsInEffect);
+            }
+
+            @Override
+            public void onStartFailure(int errorCode) {
+                Log.e( "BLE", "Advertising onStartFailure: " + errorCode );
+                super.onStartFailure(errorCode);
+            }
+        };
+
+        advertiser.startAdvertising( settings, data, advertisingCallback );
+
+
+    }
+
+}
