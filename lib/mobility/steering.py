c76a202b06fbb09a307c5ea9059e881588e67d87
tack <tackyy@gmail.com>
2016-11-12 16:56:38 -0800

    left, right, center methods in steering.py

--- lib/mobility/steering.py
+++ lib/mobility/steering.py
@@ -9,6 +9,15 @@ def steer(pwmVal):
         print("pwmVal was not a float between .1 and .2")
     print("pwmVal = {}".format(pwmVal))
 
+def left():
+    pwmDev.write('%d=%s\n'%(pin0, str(0.2)))
+
+def right():
+    pwmDev.write('%d=%s\n'%(pin0, str(0.1)))
+
+def center():
+    pwmDev.write('%d=%s\n'%(pin0, str(0.15)))
+
 if __name__ == "__main__":
     import sys
     steer(float(sys.argv[1]))
